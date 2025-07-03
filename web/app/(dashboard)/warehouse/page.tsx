98cfe1f278eee70ecc4a894a48bfcefd75631172
Xenos <743344516@qq.com>
2025-07-03 21:42:51 +0800

    add: warehouse page

--- web/app/(dashboard)/warehouse/page.tsx
+++ web/app/(dashboard)/warehouse/page.tsx
@@ -1,250 +1,155 @@
 'use client';
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Tooltip from '@mui/material/Tooltip';
-import AddIcon from '@mui/icons-material/Add';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/DeleteOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import CancelIcon from '@mui/icons-material/Close';
-import Fab from '@mui/material/Fab';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import CardActionArea from '@mui/material/CardActionArea';
 import { PageContainer } from '@toolpad/core/PageContainer';
-import {
-    GridRowsProp,
-    GridRowModesModel,
-    GridRowModes,
-    DataGrid,
-    GridColDef,
-    GridActionsCellItem,
-    GridEventListener,
-    GridRowId,
-    GridRowModel,
-    GridRowEditStopReasons,
-    GridSlotProps,
-    Toolbar,
-    ToolbarButton,
-} from '@mui/x-data-grid';
-import {
-    randomCreatedDate,
-    randomTraderName,
-    randomId,
-    randomArrayItem,
-} from '@mui/x-data-grid-generator';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
-// const roles = ['Market', 'Finance', 'Development'];
-// const randomRole = () => {
-//     return randomArrayItem(roles);
-// };
-
-const initialRows: GridRowsProp = [
-    // {
-    //     id: randomId(),
-    //     field: '',
-    //     comment: '',
-    //     filedType: '',
-    //     primaryKey: '',
-    //     isNull: ''
-    // }
+const cards = [
+    {
+        id: 1,
+        title: 'DWD层',
+        description: '数据明细层的表',
+    },
+    {
+        id: 2,
+        title: 'sap系统',
+        description: 'SAP系统的表',
+    },
+    {
+        id: 3,
+        title: 'DS数据',
+        description: 'DataService上报数据的表',
+    },
+    {
+        id: 3,
+        title: 'DS数据',
+        description: 'DataService上报数据的表',
+    },
+    {
+        id: 3,
+        title: 'DS数据',
+        description: 'DataService上报数据的表',
+    },
+    {
+        id: 3,
+        title: 'DS数据',
+        description: 'DataService上报数据的表',
+    },
 ];
 
-declare module '@mui/x-data-grid' {
-    interface ToolbarPropsOverrides {
-        setRows: (newRows: (oldRows: GridRowsProp) => GridRowsProp) => void;
-        setRowModesModel: (
-            newModel: (oldModel: GridRowModesModel) => GridRowModesModel,
-        ) => void;
-    }
-}
-
-function EditToolbar(props: GridSlotProps['toolbar']) {
-    const { setRows, setRowModesModel } = props;
-
-    const handleClick = () => {
-        const id = randomId();
-        setRows((oldRows) => [
-            ...oldRows,
-            // { id, name: '', age: '', role: '', isNew: true },
-            { id, field: '', comment: '', filedType: '', primaryKey: '', isNull: '', isNew: true },
-        ]);
-        setRowModesModel((oldModel) => ({
-            ...oldModel,
-            [id]: { mode: GridRowModes.Edit, fieldToFocus: 'name' },
-        }));
-    };
-
+function SelectActionCard() {
+    const [selectedCard, setSelectedCard] = React.useState(0);
     return (
-        <Toolbar>
-            <Tooltip title="Add record">
-                <ToolbarButton onClick={handleClick}>
-                    {/*<Fab color="primary" aria-label="add" fontSize="small" >*/}
-                        <AddIcon fontSize="small" />
-                    {/*</Fab>*/}
-                </ToolbarButton>
-            </Tooltip>
-        </Toolbar>
-    );
-}
+        <PageContainer>
+            <Typography variant="h6"
+                        gutterBottom
+                        sx={{
+                            width: '100%',
+                            display: 'grid',
+                            gridTemplateColumns: 'repeat(auto-fill, minmax(min(200px, 100%), 1fr))',
+                            gap: 2,
+                            pt:2,
+                        }}
+            >
+                分系统模型
+            </Typography>
+            <Box
+                sx={{
+                    width: '100%',
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(auto-fill, minmax(min(200px, 100%), 1fr))',
+                    gap: 2,
+                }}
+            >
 
-export default function FullFeaturedCrudGrid() {
-    const [rows, setRows] = React.useState(initialRows);
-    const [rowModesModel, setRowModesModel] = React.useState<GridRowModesModel>({});
 
-    const handleRowEditStop: GridEventListener<'rowEditStop'> = (params, event) => {
-        if (params.reason === GridRowEditStopReasons.rowFocusOut) {
-            event.defaultMuiPrevented = true;
-        }
-    };
-
-    const handleEditClick = (id: GridRowId) => () => {
-        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.Edit } });
-    };
-
-    const handleSaveClick = (id: GridRowId) => () => {
-        setRowModesModel({ ...rowModesModel, [id]: { mode: GridRowModes.View } });
-        console.log(rows);
-    };
-
-    const handleDeleteClick = (id: GridRowId) => () => {
-        setRows(rows.filter((row) => row.id !== id));
-        console.log(rows);
-    };
-
-    const handleCancelClick = (id: GridRowId) => () => {
-        setRowModesModel({
-            ...rowModesModel,
-            [id]: { mode: GridRowModes.View, ignoreModifications: true },
-        });
-
-        const editedRow = rows.find((row) => row.id === id);
-        if (editedRow!.isNew) {
-            setRows(rows.filter((row) => row.id !== id));
-        }
-    };
-
-    const processRowUpdate = (newRow: GridRowModel) => {
-        const updatedRow = { ...newRow, isNew: false };
-        setRows(rows.map((row) => (row.id === newRow.id ? updatedRow : row)));
-        return updatedRow;
-    };
-
-    const handleRowModesModelChange = (newRowModesModel: GridRowModesModel) => {
-        setRowModesModel(newRowModesModel);
-    };
-
-    const columns: GridColDef[] = [
-        // { field: 'id', headerName: 'Id', width:80, editable: false },
-        { field: 'field', headerName: '字段', width: 180, editable: true },
-        {
-            field: 'comment',
-            headerName: '备注',
-            type: 'string',
-            width: 280,
-            align: 'left',
-            headerAlign: 'left',
-            editable: true,
-        },
-        {
-            field: 'filedType',
-            headerName: '数据类型',
-            width: 100,
-            editable: true,
-            type: 'singleSelect',
-            valueOptions: ['字符串', '数值', '日期'],
-        },
-        {
-            field: 'primaryKey',
-            headerName: '主键',
-            width: 80,
-            editable: true,
-            type: 'singleSelect',
-            valueOptions: ['yes', 'no'],
-        },
-        {
-            field: 'isNull',
-            headerName: '非空',
-            width: 80,
-            editable: true,
-            type: 'singleSelect',
-            valueOptions: ['yes', 'no'],
-        },
-        {
-            field: 'actions',
-            type: 'actions',
-            headerName: '编辑',
-            width: 100,
-            cellClassName: 'actions',
-            getActions: ({ id }) => {
-                const isInEditMode = rowModesModel[id]?.mode === GridRowModes.Edit;
-
-                if (isInEditMode) {
-                    return [
-                        <GridActionsCellItem
-                            icon={<SaveIcon />}
-                            label="Save"
-                            material={{
-                                sx: {
-                                    color: 'primary.main',
+                {cards.map((card, index) => (
+                    // eslint-disable-next-line react/jsx-key
+                    <Card>
+                        <CardActionArea
+                            onClick={() => setSelectedCard(index)}
+                            data-active={selectedCard === index ? '' : undefined}
+                            sx={{
+                                height: '100%',
+                                '&[data-active]': {
+                                    backgroundColor: 'action.selected',
+                                    '&:hover': {
+                                        backgroundColor: 'action.selectedHover',
+                                    },
                                 },
                             }}
-                            onClick={handleSaveClick(id)}
-                        />,
-                        <GridActionsCellItem
-                            icon={<CancelIcon />}
-                            label="Cancel"
-                            className="textPrimary"
-                            onClick={handleCancelClick(id)}
-                            color="inherit"
-                        />,
-                    ];
-                }
-
-                return [
-                    <GridActionsCellItem
-                        icon={<EditIcon />}
-                        label="Edit"
-                        className="textPrimary"
-                        onClick={handleEditClick(id)}
-                        color="inherit"
-                    />,
-                    <GridActionsCellItem
-                        icon={<DeleteIcon />}
-                        label="Delete"
-                        onClick={handleDeleteClick(id)}
-                        color="inherit"
-                    />,
-                ];
-            },
-        },
-    ];
-
-    return (
-
-        <PageContainer
-            sx={{
-                height: 500,
-                width: '100%',
-                '& .actions': {
-                    color: 'text.secondary',
-                },
-                '& .textPrimary': {
-                    color: 'text.primary',
-                },
-            }}
-        >
-            <DataGrid
-                rows={rows}
-                columns={columns}
-                editMode="row"
-                rowModesModel={rowModesModel}
-                onRowModesModelChange={handleRowModesModelChange}
-                onRowEditStop={handleRowEditStop}
-                processRowUpdate={processRowUpdate}
-                slots={{ toolbar: EditToolbar }}
-                slotProps={{
-                    toolbar: { setRows, setRowModesModel },
+                        >
+                            <CardContent sx={{ height: '100%' }}>
+                                <Typography variant="h5" component="div">
+                                    {card.title}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    {card.description}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                ))}
+            </Box>
+            <Typography variant="h6"
+                        gutterBottom
+                        sx={{
+                            width: '100%',
+                            display: 'grid',
+                            gridTemplateColumns: 'repeat(auto-fill, minmax(min(200px, 100%), 1fr))',
+                            gap: 2,
+                            pt:5,
+                        }}
+            >
+                分系统模型
+            </Typography>
+            <Box
+                sx={{
+                    width: '100%',
+                    display: 'grid',
+                    gridTemplateColumns: 'repeat(auto-fill, minmax(min(200px, 100%), 1fr))',
+                    gap: 2,
                 }}
-                showToolbar
-            />
+            >
+                {cards.map((card, index) => (
+                    // eslint-disable-next-line react/jsx-key
+                    <Card>
+                        <CardActionArea
+                            onClick={() => setSelectedCard(index)}
+                            data-active={selectedCard === index ? '' : undefined}
+                            sx={{
+                                height: '100%',
+                                '&[data-active]': {
+                                    backgroundColor: 'action.selected',
+                                    '&:hover': {
+                                        backgroundColor: 'action.selectedHover',
+                                    },
+                                },
+                            }}
+                        >
+                            <CardContent sx={{ height: '100%' }}>
+                                <Typography variant="h5" component="div">
+                                    {card.title}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    {card.description}
+                                </Typography>
+                            </CardContent>
+                        </CardActionArea>
+                    </Card>
+                ))}
+            </Box>
         </PageContainer>
     );
 }
+
+export default SelectActionCard;
+
